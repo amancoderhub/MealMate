4194ecd894dec63e7cf565b51a7db94c8f7a4b3a
Aman Shrivastav <saurbhsrivastav6@gmail.com>
2026-02-15 22:46:36 +0530

    Change

---
+++ admin/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar/>
+      </div>
+    </div>
+  )
+}
+
+export default App
